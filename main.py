bba5824f491b70206e972995b50614a5458bd831
AlexaT2 <111393785+alexatubillo@users.noreply.github.com>
2022-08-25 23:30:23 +0000

    setting up connection

---
+++ main.py
@@ -0,0 +1,35 @@
+#Ask the user what is this kind of graph
+graph = input("It is a graphical representation of data:")
+if graph == "bar graph":
+  print("Correct!")
+else:
+  print ("wrong")
+graph = input("It is a short line which is drawn on a plane connecting the pionts on x and y coordinates.")
+if graph == "line graph":
+  print("Correct")
+else:
+  print("you put the wrong graph")
+graph = input("It is a set of points plotted on a horizontal and vertical axes.")
+if graph == "scatter plots":
+  print("great!")
+else:
+  print("It is the wrong one!")
+  graph = input("It is a circular statistical graphic, which is divided into slices to illustrate numerical proportion.")
+if graph == "pie charts":
+   print("Good job!")
+else:
+  ("Wrong!")
+graph = input("It is a display of statistical information that uses rectangles to show the frequency of data items in successive numerical intervals of equal size.")
+if graph == "histograms":
+   print("Correct, Awesome!")
+else:
+  print("you should study the meanings of the different graphs")
+
+#Ask the user to add how many people answer correct
+
+
+
+
+
+
+
